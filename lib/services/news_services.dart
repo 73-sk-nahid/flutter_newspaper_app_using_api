60ee5c956505f3ab478c579de8ffeeb94a606339
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-10-05 23:07:07 +0600

    solved errors

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,10 +1,2 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_newspaper_app_using_api/constants/constants.dart';
-class NewsService{
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: ApiUrls.baseUrl,
-      responseType
-    )
-  )
-}
