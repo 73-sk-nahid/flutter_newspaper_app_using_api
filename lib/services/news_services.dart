32463470407a1f9c0671652884a87436d3b29a6a
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-10-05 23:10:55 +0600

    complt news_service

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,2 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_newspaper_app_using_api/constants/constants.dart';
+
+class NewsService {
+  final Dio _dio = Dio(BaseOptions(
+    baseUrl: ApiUrls.baseUrl,
+    responseType: ResponseType.json,
+  ));
+
+  fetchNews() async {
+    var response = await _dio.get('language=en');
+    return response.data;
+  }
+}
